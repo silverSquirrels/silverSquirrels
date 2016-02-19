2d1094f914f5da906fa7efb9b3687eefd161dcad
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-19 15:53:36 -0600

    (fix) Add unique property to username.

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,7 +5,8 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema ({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }, 
   password: {
     type: String,
