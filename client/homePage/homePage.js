fa15cc08f1f6ba717e70037b5757f015dc504eff
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-02-19 12:34:09 -0800

    Basic leaflet map with users coords in the HomePageController. Displays on homepage.html template.

--- client/homePage/homePage.js
+++ client/homePage/homePage.js
@@ -2,6 +2,40 @@ angular.module('hikexpert.home', [])
 
 .controller('HomePageController', function($scope){
 
+   navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+
+      console.log(lat);
+      console.log(long);
+
+      var map = L.map('map').setView([lat, long], 13);
+      
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWR1bGlzOCIsImEiOiJjaWt1M2RzeW8wMDk4dnltM3h5ZXlwb24wIn0.DfujBg6HeQHg5ja-tZyYRw', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: 'pk.eyJ1IjoiZWR1bGlzOCIsImEiOiJjaWt1M2RzeW8wMDk4dnltM3h5ZXlwb24wIn0.DfujBg6HeQHg5ja-tZyYRw'
+    }).addTo(map);
+
+      L.marker([lat, long]).addTo(map)
+        .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+        var popup = L.popup();
+        function onMapClick(e) {
+        popup
+          .setLatLng(e.latlng)
+          .setContent("You clicked the map at " + e.latlng.toString())
+          .openOn(map);
+      }
+
+      map.on('click', onMapClick);
+  });
+
+
+
+
 })
 
 
