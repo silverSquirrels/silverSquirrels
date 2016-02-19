af980d48f74b5e2e64c68e159043b264773cd9c3
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-02-19 07:57:33 -0800

    Require dotenv module, config() it

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+// Cody recommended doing this
+// Makes it so the .env file is read locally to get API key
+// but on heroku if the NODE_ENV config var is set to production, app will look there
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+// this is how to access the api key
+console.log(process.env.TRAIL_API_KEY);
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
