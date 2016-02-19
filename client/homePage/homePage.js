9622e3c492918d095c1066d7671045922d101ff1
Luke Savage <csavage1994@gmail.com>
2016-02-19 06:39:11 -0600

    (chore) remove unused code

--- client/homePage/homePage.js
+++ client/homePage/homePage.js
@@ -1,14 +1,7 @@
 angular.module('hikexpert.home', [])
 
 .controller('HomePageController', function($scope){
-  var map;
-  $scope.initMap = function() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+
 })
 
 
-//AIzaSyAJellxFV2jDFD2AQdv6yiG1i6SfMnvx5o
