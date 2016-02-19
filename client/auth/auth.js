018f4883e586454d15a7cba4d2935f69f0bb06b9
Andrew Vedady <andrewvedady@Andrews-MacBook-Air.local>
2016-02-19 14:33:42 -0600

    (fix) Changed paths for /signin & /signup in App.js

---
+++ client/auth/auth.js
@@ -0,0 +1,32 @@
+angular.module('.auth', [])
+
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+
+  // $scope.signout = function () {
+  //   Auth.signout();
+  // };
+});
