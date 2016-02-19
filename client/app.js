deb50bc461b516d55bf1fa724a6047b8b23f1c36
Luke Savage <csavage1994@gmail.com>
2016-02-18 20:11:30 -0600

    added routes to signup/signin and created base files for signup/signin

--- client/app.js
+++ client/app.js
@@ -7,6 +7,14 @@ angular.module('hikexpert', [
     .when('/', {
       templateUrl: 'homePage/homepage.html',
       controller: 'HomePageController'
+    })
+    .when('/signin', {
+      templateUrl: 'app/auth/signin.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
     });
 })
 
