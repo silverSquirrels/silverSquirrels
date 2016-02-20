101a2f3b0b53058a20e23e8dc3d9c8c59f562963
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-19 20:49:33 -0600

    (feat) Add controller and route for user on sever.

---
+++ server/db/controllers/userControllers.js
@@ -0,0 +1,90 @@
+var User = require('./user.js');
+var Q = require('q');
+var jwt = require('jwt-simple');
+
+module.exports = {
+  signin: function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var findUser = Q.bind(User.findOne, User);
+    
+    findUser({username: username})
+      .then(function(user) {
+        if(!user) {
+          next(new Error('User does not exist!'));
+        } else {
+          return user.comparePassword(password)
+            .then(function(foundUser) {
+              if(foundUser) {
+                var token = jwt.encode(user, 'superskrull');
+                res.json({token: token});
+              } else {
+                return next(new Error('No User!'));
+              }
+            });
+        }
+      })
+      .fail(function(error) {
+        next(error);
+      });
+  },
+  
+  signup: function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var create;
+    var newUser;
+    
+    var findOne = Q.bind(User.findOne, User);
+    
+    // check to see if user already exists
+    findOne({username: username})
+      .then(function(user) {
+        if(user) {
+          next(new Error('User already exists!'));
+        } else {
+          // create a new user
+          create = Q.bind(User.create, User);
+          newUser = {
+            username: username,
+            password: password
+          };
+        }
+        return create(newUser);
+      })
+      .then(function(user) {
+        // create token to send back for authorization
+        var token = jwt.encode(user, 'superskrull');
+        res.json({token: token});
+      })
+      .fail(function(error) {
+        next(error);
+      });
+  },
+  
+  checkAuth: function(req, res, next) {
+    // check user authentication
+    // grab token from header
+    var token = req.headers['x-access-token'];
+    if(!token) {
+      next(new Error('No token!'));
+    } else {
+      // decode token
+      var user = jwt.decode(token, 'superskrull');
+    // check if user is in database
+    var findUser = Q.bind(User.findOne, User);
+    findUser({username: user.username})
+      .then(function(foundUser) {
+        if(foundUser) {
+          res.send(200);
+        } else {
+          res.send(401);
+        }
+      })
+      .fail(function(error) {
+        next(error);
+      });
+    }
+  }
+};
---
+++ server/db/routes/userRoutes.js
@@ -0,0 +1,5 @@
+var userController = require('./userController.js');
+
+module.exports = function(app) {
+  
+}
