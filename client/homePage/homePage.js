ff559972b7dd312cffcdd0293469f6e88b54893e
Eric Broberg <eric.edulis@gmail.com>
2016-02-22 10:59:14 -0800

    re-add access token mapbox

--- client/homePage/homePage.js
+++ client/homePage/homePage.js
@@ -44,7 +44,8 @@ angular.module('hikexpert.home', [])
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWR1bGlzOCIsImEiOiJjaWt1M2RzeW8wMDk4dnltM3h5ZXlwb24wIn0.DfujBg6HeQHg5ja-tZyYRw', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox.satellite'
+        id: 'mapbox.satellite',
+        accessToken: 'pk.eyJ1IjoiZWR1bGlzOCIsImEiOiJjaWt1M2RzeW8wMDk4dnltM3h5ZXlwb24wIn0.DfujBg6HeQHg5ja-tZyYRw'
     }).addTo(map);
 
       // Add a marker to the map
