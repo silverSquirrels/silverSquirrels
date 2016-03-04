35ba2ebb405bac2adf6fc5a74477353db876c03a
Alex Ko <alexko@utexas.edu>
2016-03-03 21:47:19 -0700

    created front end handling for adding friends

--- client/user/user.js
+++ client/user/user.js
@@ -1,7 +1,12 @@
 angular.module('hikexpert.user', [])
 
-.controller('UserController', function($scope, $rootScope, Home){
- 
-  
-
+.controller('UserController', function($scope, $rootScope, $window, Friend){
+  $scope.addFriend = function() {
+    Friend.addFriend({ newFriend: $scope.newFriend })
+    .then(function(data) {
+      console.log(data);
+    }).catch(function(err) {
+      console.error(err);
+    });
+  };
 });
