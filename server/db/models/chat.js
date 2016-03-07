08639d0de4b6395b647d28a7173032f59263b964
Alex Ko <alexko@utexas.edu>
2016-03-07 12:20:02 -0700

    created chat and message schema

---
+++ server/db/models/chat.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var chatSchema = new mongoose.Schema({
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  messages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message'
+  }],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: Date
+});
+
+chatSchema.pre('save', function(next) {
+  this.updated_at = new Date();
+  next();
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
