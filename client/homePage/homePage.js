53ba53b4e052e812c40cfe6aff6b8ce899a4eef4
JT Yim <JT@JTYim.com>
2016-03-07 01:43:04 -0500

    Render user path with mock data. User data emitting and received. Need: differentiate user paths by color?

--- client/homePage/homePage.js
+++ client/homePage/homePage.js
@@ -51,15 +51,54 @@ angular.module('hikexpert.home', [])
       });
     }, 5000);
 
-      // TODO: Fix polyline drawing
-    // socket.on('coords', function(data){
-    //   var users = Object.keys(data);
-    //   users.forEach(function(user){
-    //     data[user].forEach(function(location){
-    //       L.polyline(LatLng[location[0], location[1]], {color: 'red'}).addTo(map);
-    //     })
-    //   })
-    // })
+  
+    var polyFriendsConfig = { color: 'blue', weight: 6, opacity: 0.9 };
+    var polyUserConfig = { color: 'red', weight: 6, opacity: 0.9 };
+    var userLocs = {}
+    Socket.on('coordsResp', function(data){
+      for(var user in data){
+        if( !userLocs[user] ){
+          userLocs[user] = [];
+        }else{
+          var y = 0.0001; //mock
+          for(var i=0;i<data[user].length;i++){
+////////////   MOCK DATA SET 1    //////////////////////////////
+            if( user=='JT' ){                                                                 //
+              var x = data[user][i][0] + y;                                             //
+              y=y-0.0001;                                                                   //
+              var pt = new L.LatLng(x ,data[user][i][1])                        //
+            }else{                                                                                //
+/////////////////////////////////////////////////////////////////
+              var pt = new L.LatLng(data[user][i][0],data[user][i][1])
+            }
+            userLocs[user].push(pt)
+          }
+        }
+        Map.renderPath(userLocs[user], polyUserConfig, $scope);
+      }
+    })
+//////////     MOCK DATA SET 2  /////////////////////////////////
+    var polylinePts = [                                                                     //
+      new L.LatLng(40.65154, -73.972485),                                    //
+      new L.LatLng(40.652023, -73.972813),                                  //
+      new L.LatLng(40.652771, -73.9731),                                      //
+      new L.LatLng(40.65331, -73.973201),                                    //
+      new L.LatLng(40.653032, -73.971973),                                  //
+      new L.LatLng(40.653535, -73.972013),                                  //
+      new L.LatLng(40.655041, -73.973013),                                  //
+      new L.LatLng(40.655041, -73.973013),                                  // 
+      new L.LatLng(40.655041, -73.973013),                                  //
+      new L.LatLng(40.655041, -73.973013),                                  //
+      new L.LatLng(40.655041, -73.973013),                                  //
+      new L.LatLng(40.653067, -73.968264),                                  //
+      new L.LatLng(40.653067, -73.968264),                                  //
+      new L.LatLng(40.653067, -73.968264)                                   //
+    ];                                                                                                //
+    // setInterval(function(){                                                            //
+    //   Map.renderPath(polylinePts, polyUserConfig, $scope);       //
+    // },3000);                                                                                  //
+//////////////////////////////////////////////////////////////////
+
 
     /****************
       LISTENERS
